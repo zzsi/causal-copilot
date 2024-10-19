c4bdb69502b36ce830971b3f3d23ac08e3af6afe
fangnan <fangn06@gmail.com>
2024-10-19 16:37:52 -0700

    Update Initialize_state.py module.

--- global_setting/Initialize_state.py
+++ global_setting/Initialize_state.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
-from typing import List, Dict, Optional, Union
+from typing import List, Dict, Optional, Union, Any
 import argparse
 import pandas as pd
+import ast
 import numpy as np
 import os
 import json
@@ -48,7 +49,7 @@ class Logging:
 class Algorithm:
     selected_algorithm: Optional[str] = None
     selected_reason: Optional[str] = None
-    algorithm_candidates: Dict[Dict] = field(default_factory=dict)
+    algorithm_candidates: Dict[Dict, Dict[str, Any]] = field(default_factory=dict)
     algorithm_candidate_name: Optional[str] = None
     algorithm_candidate_desc: Optional[str] = None
     algorithm_candidate_just: Optional[str] = None
@@ -71,40 +72,6 @@ class GlobalState:
     algorithm: Algorithm = field(default_factory=Algorithm)
     results: Results = field(default_factory=Results)
 
-def global_state_initialization(data_path: str, user_query: str = None) -> GlobalState:
-    global_state = GlobalState()
-    global_state.user_data.initial_query = user_query
-    # TODO: initial the state with the exaction information from user query
-
-    return global_state
-
-def load_data(global_state: GlobalState, args: argparse.Namespace):
-    if args.simulation_mode == "online":
-        simulation_manager = SimulationManager(args)
-        config, data, graph = simulation_manager.generate_dataset()
-    elif args.simulation_mode == "offline":
-        if args.data_mode == "simulated":
-            config, data, graph = load_local_data(args.data_file)
-        elif args.data_mode == "real":
-            data = pd.read_csv(args.data_file)
-            graph = None
-        else:
-            raise ValueError("Invalid data mode. Please choose 'real' or 'simulated'.")
-    else:
-        raise ValueError("Invalid simulation mode. Please choose 'online' or 'offline'.")
-    
-    global_state.user_data.raw_data = data
-    global_state.user_data.ground_truth = graph
-
-    # hard-coded heterogeneous and domain index, later would be set by the user query
-    if 'domain_index' in data.columns:
-        if data.nunique(axis=0)['domain_index'] > 1:
-            global_state.statistics.heterogeneous = True
-        else:
-            global_state.statistics.heterogeneous = False
-        global_state.statistics.domain_index = 'domain_index'
-
-    return global_state
 
 def load_local_data(directory: str):
     # Xinyue Wang Implemented
@@ -141,3 +108,105 @@ def load_local_data(directory: str):
         graph = None
 
     return config, data, graph
+
+
+def load_data(global_state: GlobalState, args: argparse.Namespace):
+    if args.simulation_mode == "online":
+        simulation_manager = SimulationManager(args)
+        config, data, graph = simulation_manager.generate_dataset()
+    elif args.simulation_mode == "offline":
+        if args.data_mode == "simulated":
+            config, data, graph = load_local_data(args.data_file)
+        elif args.data_mode == "real":
+            data = pd.read_csv(args.data_file)
+            graph = None
+        else:
+            raise ValueError("Invalid data mode. Please choose 'real' or 'simulated'.")
+    else:
+        raise ValueError("Invalid simulation mode. Please choose 'online' or 'offline'.")
+
+    global_state.user_data.raw_data = data
+    global_state.user_data.ground_truth = graph
+
+    # hard-coded heterogeneous and domain index, later would be set by the user query
+    if 'domain_index' in data.columns:
+        if data.nunique(axis=0)['domain_index'] > 1:
+            global_state.statistics.heterogeneous = True
+        else:
+            global_state.statistics.heterogeneous = False
+        global_state.statistics.domain_index = 'domain_index'
+
+    return global_state
+
+
+
+
+def global_state_initialization(data_path: str, user_query: str = None, args) -> GlobalState:
+    global_state = GlobalState()
+    #user_query = "I’m analyzing a biological dataset with 2000 samples and 12 features. The data has some missing values, and I assume the relationships between the variables are linear. I believe the error terms follow a Gaussian distribution. The dataset is homogeneous, and there’s no specific domain index. I’d like to use the NOTEARS algorithm for causal discovery. Can you guide me through the process?"
+
+    global_state.user_data.initial_query = user_query
+
+    # Extract information from user queries
+    from openai import OpenAI
+
+    # organization = "org-5NION61XDUXh0ib0JZpcppqS"
+    # project = "proj_Ry1rvoznXAMj8R2bujIIkhQN"
+    # apikey = "sk-l4ETwy_5kOgNvt5OzHf_YtBevR1pxQyNrlW8NRNPw2T3BlbkFJdKpqpbcDG0IhInYcsS3CXdz_EMHkJO7s1Bo3e4BBcA"
+
+    client = OpenAI(organization=args.organization, project=args.project, api_key=args.apikey)
+    prompt = (f"Using the query that I provided: {user_query} \n\n, "
+              "and extract the following information for me based on the format of the example I provided. "
+              "For example, if my query is: \n\n "
+              "The relationship between the variables are assumed to be linear (for this kind of information, you just need to extract if the relationship is linear or non-linear).\n\n "
+              "The errors are assumed to be Gaussian (for this kind of information, you just need to extract if the error follows Gaussian distribution or not).\n\n "
+              "There is missingness in the dataset (for this kind of information, you just need to extract if there is missing values or not in the dataset).\n\n "
+              "The suggested significant level for in the analysis is 0.05 (it should be a value that is larger than 0 and smaller than 1).\n\n "
+              "The datatype is mixture, which means it contain both continuous and categorical variables. Other options for data type is 'Continuous' or 'Category'.\n\n "
+              "The data is heterogeneous (for this kind of information, you just need to extract if the data is heterogeneous or not).\n\n "
+              "The feature which represents the domain index is 'sea_level' in the dataset. (for this kind of information, you just need to extract the column name that stands for the domain index in the dataset).\n\n"
+              "I would like to use PC to do causal discovery. Other options for algorithm include FCI, CDNOD, GES, NOTEARS, DirectLiNGAM, ICALiNGAM. \n\n"
+              "Then you should give me the output that is in a dict format: \n\n"
+              "{'linearity': True, 'gaussian_error': True, 'missingness': True, 'alpha': 0.05, 'data_type': 'Mixture', 'heterogeneous': True, 'domain_index':'sea_level', 'selected_algorithm': 'PC'}.  \n\n"
+              "If the information does not match the options I provided above or the queries do not provide such information, you would give value of None for such key !"
+              "Just give me the output in a dict format, do not provide other information! \n\n")
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    # The output from GPT is str
+    info_extracted = response.choices[0].message.content
+    info_extracted = info_extracted.replace('```python', '').replace('```', '').strip()
+
+    # Convert to dict
+    info_extracted = ast.literal_eval(info_extracted)
+
+    # Assign extracted information from user queries to global_stat
+    global_state.statistics.linearity = info_extracted["linearity"]
+    global_state.statistics.gaussian_error = info_extracted["gaussian_error"]
+    global_state.statistics.missingness = info_extracted["missingness"]
+    global_state.statistics.alpha = info_extracted["alpha"]
+    global_state.statistics.data_type = info_extracted["data_type"]
+    global_state.statistics.heterogeneous = info_extracted["heterogeneous"]
+    global_state.statistics.domain_index = info_extracted["domain_index"]
+    global_state.algorithm.selected_algorithm = info_extracted["selected_algorithm"]
+
+    # Load Data
+    # _, data, graph = load_local_data(data_path)
+    # global_state.user_data.raw_data = data
+    # global_state.user_data.ground_truth = graph
+
+    # Statistics Information Collection
+
+
+    return global_state
+
+
+
+
+
